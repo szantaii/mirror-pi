3a0553999abe1ee365e3450938cca86cdfbf8b8b
szantaii <szantaii@gmail.com>
2015-01-01 18:06:47 +0100

    Revert "Added licensing information"
    This reverts commit fe68d0d60ee9506a02716b301cd59d3b48003fd9.

--- webapp/webapp.py
+++ webapp/webapp.py
@@ -1,10 +1,5 @@
 #!/usr/bin/env python
 
-# Mirror π
-# 
-# Copyright (c) 2014 Istvan Szantai <szantaii at sidenote dot hu>
-# Released under the MIT License (LICENSE).
-
 import gtk
 import webkit
 import os
